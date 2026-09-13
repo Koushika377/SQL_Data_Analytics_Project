78ef639cd265d592983388f1d43ef43ace620e66
Koushika377 <koushikayalam@gmail.com>
2026-09-14 00:28:04 +0530

    Update and rename date_exploration.sql to 3_date_exploration.sql

--- scripts/EDA/3_date_exploration.sql
+++ scripts/EDA/3_date_exploration.sql
@@ -1,10 +1,14 @@
+/*
+explore boundaries of the data, range
+*/
+
 -- earliest and latest dates
 -- scope of the data & the timespan
 select 
-min(order_date) as first_order_date,
-max(order_date) as last_order_date,
-datediff(month,min(order_date),max(order_date)) as order_range_months
-from gold.fact_sales
+  min(order_date) as first_order_date,
+  max(order_date) as last_order_date,
+  datediff(month,min(order_date),max(order_date)) as order_range_months
+from gold.fact_sales;
 
 -- find the youngest and oldest customer
 select
@@ -12,4 +16,4 @@ min(birthdate) as oldest_birthdate,
 datediff(year,min(birthdate),getdate()) as oldest_age,
 max(birthdate) as youngest_birthdate,
 datediff(year,max(birthdate),getdate()) as youngest_age
-from gold.dim_customers
+from gold.dim_customers;
